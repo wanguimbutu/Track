13a63e3496d9caaccf00e718fd379e40ad18dc3d
wanguimbutu <wanguimbutu@gmail.com>
2025-06-27 08:28:31 +0300

    Revert "update delivery note helper"
    This reverts commit fb64b1a658caf9b50e1474b8670100f89999cec5.

--- track/track/doctype/delivery_return/delivery_return.js
+++ track/track/doctype/delivery_return/delivery_return.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2025, wanguimbutu and contributors
 // For license information, please see license.txt
 
-
 frappe.ui.form.on('Delivery Return', {
     refresh(frm) {
         // Only show the button if the document is submitted
